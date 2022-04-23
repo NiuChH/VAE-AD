ac9258bd2f912b015401bd7a1068055077f4d46d
Chenhao niu <niu296156@me.com>
2022-04-23 16:06:46 -0400

    fix nf

--- models/VAE_NF_detector.py
+++ models/VAE_NF_detector.py
@@ -73,7 +73,7 @@ class VAE_NF_Detector(nn.Module):
             ]
         else:
             raise NotImplementedError()
-        prior = torch.distributions.MultivariateNormal(torch.zeros(n_bottleneck, device=device),
+        self.prior = torch.distributions.MultivariateNormal(torch.zeros(n_bottleneck, device=device),
                                                        torch.eye(n_bottleneck, device=device))
         encoder_cnns = []
         cur_size = image_size
@@ -97,10 +97,11 @@ class VAE_NF_Detector(nn.Module):
             decoder_cnns.append(nn.ConvTranspose2d(arg[1], arg[0], *arg[2:]))
             decoder_cnns.append(nn.BatchNorm2d(arg[0]))
             decoder_cnns.append(nn.ReLU(True))
-        decoder_nn = nn.Sequential(*decoder_cnns[:-2])
+        decoder_cnns.append(nn.Conv2d(x_emb_dim, 2*in_channels, 1, 1))
+        decoder_nn = nn.Sequential(*decoder_cnns)
 
-        encoder = nf.distributions.NNDiagGaussian(encoder_nn)
-        decoder = nf.distributions.NNBernoulliDecoder(decoder_nn)
+        self.encoder = nf.distributions.NNDiagGaussian(encoder_nn)
+        self.decoder = nf.distributions.NNDiagGaussian(decoder_nn)
 
         if flow_type == 'Planar':
             flows = [nf.flows.Planar((n_bottleneck,)) for k in range(n_flows)]
@@ -118,48 +119,91 @@ class VAE_NF_Detector(nn.Module):
                     flows += [nf.flows.MaskedAffineFlow(1 - b, t, s)]
         else:
             raise NotImplementedError
+        self.flows = flows
 
-        self.nfm = nf.NormalizingFlowVAE(prior, encoder, flows, decoder)
+        # self.nfm = nf.NormalizingFlowVAE(prior, encoder, flows, decoder)
 
         self.result_cache = easydict.EasyDict({
-            'mu_z': None, 'logvar_z': None, 'mu_x': None, 'logvar_x': None, 'nll_pixel': None
+            'mu_x': None, 'logvar_x': None, 'nll_pixel': None
         })
         self.loss_cache = easydict.EasyDict({
             'nll': None, 'kl_qp': None, 'loss': None
         })
 
     def write_loss(self, writer, epoch):
-        writer.add_scalar('mean_log_q', self.loss_cache.mean_log_q.item(), epoch)
-        writer.add_scalar('mean_log_p', self.loss_cache.mean_log_p.item(), epoch)
+        writer.add_scalar('nll', self.loss_cache.nll.item(), epoch)
+        writer.add_scalar('kl_qp', self.loss_cache.kl_qp.item(), epoch)
         writer.add_scalar('loss', self.loss_cache.loss.item(), epoch)
 
     def write_hist(self, writer, epoch):
-        writer.add_histogram('z', self.result_cache.z, epoch)
+        # writer.add_histogram('mu_z', self.result_cache.mu_z, epoch)
+        # writer.add_histogram('logvar_z', self.result_cache.logvar_z, epoch)
+        writer.add_histogram('mu_x', self.result_cache.mu_x, epoch)
+        writer.add_histogram('logvar_x', self.result_cache.logvar_x, epoch)
+
 
     def write_reconstructions(self, writer, epoch):
-        reconstructions = self.result_cache.z
+        mu_x = self.result_cache.mu_x
+        logvar_x = self.result_cache.logvar_x
+        reconstructions = sample_gaussian(mu_x, logvar_x)
         writer.add_image(
             'Reconstructed Sample',
             torchvision.utils.make_grid(reconstructions.clamp(0., 1.)),
             epoch, dataformats='CHW')
+        writer.add_image(
+            'Mean',
+            torchvision.utils.make_grid(mu_x.clamp(0., 1.)),
+            epoch, dataformats='CHW')
 
     def forward(self, x, *args, **kwargs):
-        z, log_q, log_p = self.nfm(x, self.num_samples)
-        mean_log_q = torch.mean(log_q)
-        mean_log_p = torch.mean(log_p)
-        loss = mean_log_q - mean_log_p
-        self.result_cache.update({
-            'z': z.mean(dim=1),
-            'loc': -log_p.mean(dim=1)
-        })
+        # z, log_q, log_p = self.nfm(x, self.num_samples)
+        num_samples = self.num_samples
+        z, log_q = self.encoder(x, num_samples=num_samples)
+        # Flatten batch and sample dim
+        z = z.view(-1, *z.size()[2:])
+        log_q = log_q.view(-1, *log_q.size()[2:])
+        # size: [batch_size*num_samples,]
+
+        for flow in self.flows:
+            z, log_det = flow(z)
+            log_q -= log_det
+        log_p = self.prior.log_prob(z)  # [batch_size*num_samples]
+
+        mean_std = self.decoder.net(z)
+        n_hidden = mean_std.size()[1] // 2
+        mu_x = mean_std[:, :n_hidden, ...]
+        mu_x = mu_x.view(-1, num_samples, *mu_x.size()[1:])
+        logvar_x = mean_std[:, n_hidden:(2 * n_hidden), ...]
+        var = torch.exp(logvar_x)
+        var = var.view(-1, num_samples, *var.size()[1:])
+        log_p_x_given_z = - 0.5 * torch.prod(torch.tensor(x.size()[1:])) * np.log(2 * np.pi) \
+            - 0.5 * torch.sum(torch.log(var) + (x.unsqueeze(1) - mu_x) ** 2 / var, dim=2)
+        # [batch_size, num_samples, W, H]
+
+        # log_p_x_given_z = self.decoder.log_prob(x, z)  # [batch_size*num_samples, ]
+        # Separate batch and sample dimension again
+        # z = z.view(-1, num_samples, *z.size()[1:])
+
+        mean_log_q = torch.mean(log_q)  # mean over [batch_size*num_samples,]
+        mean_log_p = torch.mean(log_p)  # mean over [batch_size*num_samples,]
+
+        mean_log_p_x_given_z = torch.sum(log_p_x_given_z) / log_p_x_given_z.shape[0] * log_p_x_given_z.shape[1]
+        # mean over [batch_size, num_samples]
+        # sum over [W, H]
+        kl_qp = (mean_log_q - mean_log_p)
+        nll = - mean_log_p_x_given_z
+        loss = nll + kl_qp
         self.loss_cache.update({
-            'mean_log_q': mean_log_q, 'mean_log_p': mean_log_p, 'loss': loss,
-            # 'score': log_p.mean(dim=1).sum()  # mean over all samples
+            'nll': nll, 'kl_qp': kl_qp, 'loss': loss
+        })
+        self.result_cache.update({
+            'nll_pixel': -log_p_x_given_z.detach().cpu(),
+            'mu_x': mu_x.detach().cpu(), 'logvar_x': logvar_x.detach().cpu()
         })
         return loss
 
     def get_ano_score(self):
-        return self.loss_cache.loss.detach().cpu().numpy()
+        return self.loss_cache.nll.detach().cpu().numpy()
 
     def get_ano_loc_score(self):
-        return self.result_cache.loc.detach().cpu().numpy()
+        return self.result_cache.nll_pixel.detach().cpu().numpy()
